1621a17197d6488c8509fd9a4200ebfeed460098
camilo <camilocstr2020@gmail.com>
2021-09-20 23:27:38 -0500

    Destructurar elementos, operador de progpagacion, template literales y las variables let y const.

--- src/es6/index.js
+++ src/es6/index.js
@@ -25,3 +25,58 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello } ${world}`;
 console.log(epicPhrase2)
+
+let lorem = "Quiero construir una frase epica \n"
+ + "otra frase epica!"
+
+let lorem2 =`otra frase epica x2 hahha
+ahora es otra frase epica
+`;
+
+console.log(lorem)
+console.log(lorem2);
+
+let person = {
+    'name': 'Camilo',
+    'age': 24,
+    'country': 'Colombia'
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.country)
+
+
+//Destructurar elementos
+//Puedo traer los elementos que necesito
+let {country} = person;
+console.log(country)
+
+//Operador de propagacion ...
+let team1 = ['Camilo', 'Andres', 'Pepito']
+let team2 = ['Pepita', 'Juanita', 'Lupita']
+
+let education = ['David', ...team1, ...team2]
+console.log(education)
+
+// Utilizar let 
+
+// disponible de forma global
+// let solo disponible en el bloque de codigo definido.
+
+var hola = 'Hola';
+{
+    var globalVar = 'Global Var';
+}
+
+{
+    let globalLet = 'Global let';
+    console.log(globalLet)
+}
+
+console.log(globalVar);
+
+//const no cambia su valor
+const a = 'b';
+a = 'a';
+console.log(a);
